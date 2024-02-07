84b49e8047fb9cc20746f2c5401f6725e036fc0d
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-02-07 01:01:36 +0100

    Added project for testing whatever comes up while practicing

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout <<"Unsigned long long: " << sizeof(unsigned long long) << std::endl;
+    std::cout << "Unsigned long long int: " << sizeof(unsigned long long int) << std::endl;
+
+    return 0;
+}
