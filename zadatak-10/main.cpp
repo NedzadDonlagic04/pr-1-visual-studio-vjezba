cf2cfd5a52c71dfeb112dfab09d9222a13de03c7
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-02-20 00:15:31 +0100

    Added practice problem 10

---
+++ zadatak-10/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<random>
+
+int main() {
+	std::random_device dev{};
+	std::mt19937 rng{ dev() };
+	std::uniform_int_distribution<std::mt19937::result_type> cardDrawDist{ 1, 14 };
+
+	int card1{}, card2{}, card3{};
+	unsigned int counter{ 0u };
+
+	do {
+		card1 = cardDrawDist(rng);
+		card2 = cardDrawDist(rng);
+		card3 = cardDrawDist(rng);
+
+		counter++;
+	} while (std::abs(card1 - card2) != 2 || std::abs(card2 - card3) != 2);
+
+	std::cout << "Ukupno pokusaja: " << counter << std::endl;
+	std::cout << "Krajnja kombinacija: " << card1 << ' ' << card2 << ' ' << card3 << std::endl;
+
+	return 0;
+}
