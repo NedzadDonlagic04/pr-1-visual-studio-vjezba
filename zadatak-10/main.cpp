e4bf0dcba5e191399a4e4234624e68163d86bffe
NedzadDonlagic04 <nedzaddonlagic04@gmail.com>
2024-02-20 16:10:53 +0100

    Updated code since I got the problem description wrong

--- zadatak-10/main.cpp
+++ zadatak-10/main.cpp
@@ -1,24 +1,46 @@
 #include<iostream>
 #include<random>
+#include<array>
+#include<algorithm>
+
+constexpr std::size_t CARDS_TO_DRAW{ 5 };
+
+[[nodiscard]] bool isSameDistanceBetweenAdjancentElements(std::array<int, CARDS_TO_DRAW>::iterator, const std::array<int, CARDS_TO_DRAW>::iterator);
 
 int main() {
 	std::random_device dev{};
 	std::mt19937 rng{ dev() };
 	std::uniform_int_distribution<std::mt19937::result_type> cardDrawDist{ 1, 14 };
 
-	int card1{}, card2{}, card3{};
+	std::array<int, CARDS_TO_DRAW> cards{};	
 	unsigned int counter{ 0u };
 
 	do {
-		card1 = cardDrawDist(rng);
-		card2 = cardDrawDist(rng);
-		card3 = cardDrawDist(rng);
+		std::generate(cards.begin(), cards.end(), [&cardDrawDist, &rng]() { return cardDrawDist(rng); });
 
 		counter++;
-	} while (std::abs(card1 - card2) != 2 || std::abs(card2 - card3) != 2);
+	} while (!isSameDistanceBetweenAdjancentElements(cards.begin(), cards.end()));
 
 	std::cout << "Ukupno pokusaja: " << counter << std::endl;
-	std::cout << "Krajnja kombinacija: " << card1 << ' ' << card2 << ' ' << card3 << std::endl;
+	std::cout << "Krajnja kombinacija: ";  
+	std::for_each(cards.begin(), cards.end(), [](const int card) { std::cout << card << ' '; });
+	std::cout<<std::endl;
 
 	return 0;
+}
+
+bool isSameDistanceBetweenAdjancentElements(std::array<int, CARDS_TO_DRAW>::iterator itBegin, const std::array<int, CARDS_TO_DRAW>::iterator itEnd) {
+	if (itBegin + 1 == itEnd) {
+		return true;
+	}
+
+	const unsigned int distance = std::abs(*itBegin - *(itBegin + 1));
+
+	for (itBegin++; itBegin + 1 != itEnd; itBegin++) {
+		if (distance != std::abs(*itBegin - *(itBegin + 1))) {
+			return false;
+		}
+	}
+
+	return true;
 }
